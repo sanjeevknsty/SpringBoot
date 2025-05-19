59ad816092ce5dc182914cd0ceb76f5b6e1065ca
sanjeevknsty <121655617+sanjeevknsty@users.noreply.github.com>
2025-05-19 17:52:37 +0530

    Update HelloWebController.java

--- src/main/java/com/example/HelloWorldDemo/HelloWebController.java
+++ src/main/java/com/example/HelloWorldDemo/HelloWebController.java
@@ -1,25 +1,25 @@
-//package com.example.HelloWorldDemo;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.ui.Model;
-//import org.springframework.web.bind.annotation.GetMapping;
-//
-//@Controller
-//public class HelloWebController {
-//	@GetMapping("/web")
-//	public String hello() {
-//		return "hello";
-//	}
-//	@GetMapping("/web/message")
-//	public String message(Model model) {
-//		model.addAttribute("Message","This is a Custon Message");
-//		return "message";
-//	}
-//	
-//	
-//}
-//
-//
-//
+package com.example.HelloWorldDemo;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloWebController {
+	@GetMapping("/web")
+	public String hello() {
+		return "hello";
+	}
+	@GetMapping("/web/message")
+	public String message(Model model) {
+		model.addAttribute("Message","This is a Custon Message");
+		return "message";
+	}
+	
+	
+}
+
+
+
